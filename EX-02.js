26b002b969e1b53228e168b32c66ea29d2090804
Jones <161722209+ConesHebert@users.noreply.github.com>
2024-08-27 13:20:29 +0000

    2. Crie a classe Pessoa e o método cracha(), que imprime apenas Convidado e o nome da pessoa. Na classe Aluno deve imprimir o nome e o RA e na classe AlunoGraduação deve imprimir o nome do aluno e nome do curso.

---
+++ EX-02.js
@@ -0,0 +1,28 @@
+class pessoa{
+    
+    constructor(nome){
+        this.nome
+    }
+   
+    crasha(){
+        console.log('convidado' + this.nome)
+    }
+}
+class aluno extends pessoa{
+    super(nome){
+        this.RA
+    }
+    crasha(){
+        console.log('convidado' + this.nome + 'RA' + this.RA)
+    }
+}
+            
+class graduacao extends pessoa{
+    super(nome){
+        this.curso
+    }
+    crasha(){
+        console.log('convidado' + this.nome + 'Curso' + this.curso)
+    }   
+
+}
